7f3e33665eefc57064872de3d52abcb29b4adece
md-alif007 <mohammadalif043@gmail.com>
2025-11-15 10:51:41 +0600

    toggle button

--- react-world-on-the-go/src/components/Country/Country.jsx
+++ react-world-on-the-go/src/components/Country/Country.jsx
@@ -5,12 +5,17 @@ const Country = ({ country }) => {
     const [Visited, setVisited] = useState(false)
 
     const handleVisited = () => {
-        setVisited(true);
+        // if (Visited === true) {
+        //     setVisited(false);
+        // } else {
+        //     setVisited(true);
+        // }
+        setVisited(!Visited);
 
     }
 
     return (
-        <div className='country'>
+        <div className={`country ${Visited && `country-visited`}`}>
             <div>Name : {country.name.common}</div>
             <img src={country.flags.png} alt="" />
             <button onClick={handleVisited}>

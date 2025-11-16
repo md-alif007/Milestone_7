a4536c345748bd942de49b7d022d87daa5575574
md-alif007 <mohammadalif043@gmail.com>
2025-11-16 15:05:09 +0600

    practicing react import export and components

---
+++ water-bottles/src/App.jsx
@@ -0,0 +1,23 @@
+
+import { Suspense } from 'react'
+import './App.css'
+import Bottles from './components/bottles/bottles'
+
+const bottlesPromise = fetch('./bottles.json')
+  .then(res => res.json());
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Buy awesome water bottles</h1>
+      <Suspense fallback={<h3>Bottles are loading....</h3>}>
+        <Bottles bottlesPromise = {bottlesPromise}></Bottles>
+      </Suspense>
+
+    </>
+  )
+}
+
+export default App

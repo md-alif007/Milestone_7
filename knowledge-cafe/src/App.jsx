f9ab3b4171559944d3e1b0b1afefc2ad9d3804f1
md-alif007 <mohammadalif043@gmail.com>
2025-11-20 11:54:01 +0600

    trying to do something

--- knowledge-cafe/src/App.jsx
+++ knowledge-cafe/src/App.jsx
@@ -1,13 +1,22 @@
+import { Suspense } from 'react';
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Blogs from './Components/Blogs/Blogs';
 
+
+const knowledgePromise = fetch('knowledge.json').then(res => res.json());
+console.log(knowledgePromise);
 
 function App() {
-  
+
+
 
   return (
     <>
       <Navbar></Navbar>
+      <Suspense fallback={<h3>Informations are loading....</h3>}>
+          <Blogs knowledgePromise={knowledgePromise}></Blogs>
+      </Suspense>
     </>
   )
 }
---
+++ knowledge-cafe/src/Components/Blogs/Blogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = ({knowledgePromise}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;

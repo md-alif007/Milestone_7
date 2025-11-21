58a1aa7c669c9e7cf77c631f7cac2aa9b4eb9eed
md-alif007 <mohammadalif043@gmail.com>
2025-11-21 21:11:21 +0600

    adding book mark button

--- knowledge-cafe/src/Components/Blog/Blog.jsx
+++ knowledge-cafe/src/Components/Blog/Blog.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { FaBookmark } from "react-icons/fa6";
 
-const Blog = ({blog}) => {
+const Blog = ({ blog }) => {
     return (
         <div>
-            <h1>{blog.title}</h1>
+            <div className="border-b-2">
+                <figure className="px-10 pt-10">
+                    <img
+                        src={blog.cover}
+                        alt=""
+                        className="rounded-xl" />
+                </figure>
+                <div className='mt-3 flex items-center justify-center gap-3'>
+                    <img className='w-[50px]' src={blog.author_img} alt="" />
+                    <h1>{blog.author}</h1>
+                </div>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">{blog.title}</h2>
+                    <div className='flex gap-3'>
+                        {
+                            blog.hashtags.map((hash) => <p>#{hash}</p>)
+                        }
+                    </div>
+                    <div className="card-actions">
+                        <button className="btn btn-primary">Mark As Read</button>
+                        <button className="btn btn-primary"><FaBookmark />Book mark</button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
